fdeda8c39996cc144f1f8c9e7998f8cabe74a069
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-05-25 15:13:34 -0400

    used @ModelAttribute to receive the data

--- src/main/java/sheridan/tetervak/hellosheridan/controller/NameController.java
+++ src/main/java/sheridan/tetervak/hellosheridan/controller/NameController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import sheridan.tetervak.hellosheridan.domain.User;
 
@@ -20,12 +21,11 @@ public class NameController {
     }
 
     @GetMapping("/Output")
-    public String output(@RequestParam String firstName,
-                         @RequestParam String lastName,
+    public String output(@ModelAttribute User user,
                          Model model) {
 
         logger.trace("output() was called");
-        var user = new User(firstName,lastName);
+//        var user = new User(firstName,lastName);
         logger.debug("user = " + user);
         model.addAttribute("user", user);
         return "Output";
